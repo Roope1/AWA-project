dfa3d31e42c8712dfd7c89ca208b9b2c0c6ac13c
Roope Turkki <roopeh.turkki@gmail.com>
2024-02-11 15:50:41 +0200

    User model formatted

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,10 +4,10 @@ import { model, Schema, InferSchemaType } from "mongoose";
 const mongoose = require('mongoose');
 
 const userSchema = new Schema({
-    name: {type: String, required: true},
-    username: {type: String, required: true},
-    profilePic: {type: String, required: false},
-    password: {type: String, required: true},
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    profilePic: { type: String, required: false },
+    password: { type: String, required: true },
 }, { timestamps: true })
 
 export type User = InferSchemaType<typeof userSchema>;
