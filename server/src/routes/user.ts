06bf535d8d8c4629c28324f0a1afb63d888ac826
Roope Turkki <roopeh.turkki@gmail.com>
2024-02-13 16:27:06 +0200

    improved random user endpoint

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -3,6 +3,7 @@ var router = express.Router();
 
 import { User } from "../models/User"
 
+/* Authenticate user */
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     console.log("req.user", req.user)
     User.findOne({ username: req.user })
@@ -15,19 +16,28 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
     })
 })
 
+/* Used to get the random users for "swiping" */
 router.get('/random', (req: Request, res: Response, next: NextFunction) => {
     // get the authenticated users matches
     User.findOne({ username: req.user })
     .then((user: User | null) => {
         if (!user) throw new Error;
-        // find all users that the user has not matched with
-        User.find({ _id: {$nin: user.matches}})
+        // find all users that the user has not matched with and not the authorized user 
+        User.find({
+            $and: [
+                { _id: {$nin: user.matches}},
+                { username: {$ne: req.user }}
+            ]
+        })
         .then((users: User[] | null) => {
             if (!users){
-                // handle this
+                res.json({msg: "No more users"})
             } else {
-                // TODO: parse this info before sending (dont send password or other stuff like that)
-                res.send(users[Math.floor((Math.random() * users.length))])
+                // typed to partial to be able to "break out" of the User type mold (to not send the password anywhere)
+                let randomUser: any = users[Math.floor((Math.random() * users.length))] as any
+                // set the password to undefined as that wont be sent in the response (using this as delete keyword didn't work)
+                randomUser.password = undefined;
+                res.send(randomUser);
             }
         })
     })
